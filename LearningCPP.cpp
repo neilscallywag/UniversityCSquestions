aba33c99186b688cb75a40571d13d3138c846879
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-10-20 01:51:56 +0800

    Update LearningCPP.cpp

--- LearningCPP.cpp
+++ LearningCPP.cpp
@@ -6,8 +6,12 @@ void increment(int& const value) {
 class Player { //new data type eg int var it will be Player var
 public:
 	int x, y, speed;
+	Player(int X, int Y, int S) {
+		x = X;
+		y = Y;
+		speed = S;
 
-
+	}
 	void Move(int& xa, int& ya) {
 		x += xa * speed;
 		y += ya * speed;
@@ -16,48 +20,41 @@ public:
 
 class Log {
 public:
-	enum Level {
+	enum class Level : unsigned char {
 		LError = 0, LWarning, LInfo
 	};
 private:
-	Level m_LogLevel = LInfo;
+	Level m_LogLevel = Level::LInfo;
 public:
 	void SetLevel(Level level) {
 		m_LogLevel = level;
 	}
 	void ErrorM(const char* message)
 	{
-		if (m_LogLevel >= LError)
+		if (m_LogLevel >= Level::LError)
 			std::cout << "[Error]" << message << std::endl;
 	}
 	void Warn(const char* message)
 	{
-		if (m_LogLevel >= LWarning)
+		if (m_LogLevel >= Level::LWarning)
 			std::cout << "[Warning]" << message << std::endl;
 	}
 	void Info(const char* message)
 	{
-		if (m_LogLevel >= LInfo)
+		if (m_LogLevel >= Level::LInfo)
 			std::cout << "[Info]" << message << std::endl;
 	};
 };
 int main() {
 	/// LEARNING CLASSES;
 
-	Player player;
-	player.x = 5;
-	player.y = 5;
-	player.speed = 1;
+	Player player(5, 5, 1);
 
-	Player player2;
-	player2.x = 2;
-	player2.y = 2;
-	player2.speed = 1;
-
-	for (static int i; i < 10; i++) {
-		increment(player.x);
-		increment(player2.x);
-	}
+	Player player2(2, 2, 1);
+		for (static int i; i < 10; i++) {
+			increment(player.x);
+			increment(player2.x);
+		}
 
 	player.Move(player.x, player.y);
 	player2.Move(player2.x, player2.y);
@@ -68,8 +65,8 @@ int main() {
 
 	/// LEARNING ENUMS
 	Log log;
-	log.SetLevel(Log::LError);
-	log.Warn("Hello!");
-	log.ErrorM("World!");
+	log.SetLevel(Log::Level::LError);
+	log.Warn("a!");
+	log.ErrorM("q!");
 
 }
