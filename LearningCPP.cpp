11d50e330e42ffce15ba8db45a4459661c44fba4
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-10-20 00:03:39 +0800

    Update LearningCPP.cpp

--- LearningCPP.cpp
+++ LearningCPP.cpp
@@ -13,7 +13,36 @@ public:
 		y += ya * speed;
 	}
 };
+
+class Log {
+public:
+	enum Level {
+		LError = 0, LWarning, LInfo
+	};
+private:
+	Level m_LogLevel = LInfo;
+public:
+	void SetLevel(Level level) {
+		m_LogLevel = level;
+	}
+	void ErrorM(const char* message)
+	{
+		if (m_LogLevel >= LError)
+			std::cout << "[Error]" << message << std::endl;
+	}
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LWarning)
+			std::cout << "[Warning]" << message << std::endl;
+	}
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LInfo)
+			std::cout << "[Info]" << message << std::endl;
+	};
+};
 int main() {
+	/// LEARNING CLASSES;
 
 	Player player;
 	player.x = 5;
@@ -33,6 +62,14 @@ int main() {
 	player.Move(player.x, player.y);
 	player2.Move(player2.x, player2.y);
 
-	std::cout << player.x  << " " << player.y << std::endl;
-	std::cout << player2.x  << " "  << player2.y << std::endl;
+	std::cout << player.x << " " << player.y << std::endl;
+	std::cout << player2.x << " " << player2.y << std::endl;
+
+
+	/// LEARNING ENUMS
+	Log log;
+	log.SetLevel(Log::LError);
+	log.Warn("Hello!");
+	log.ErrorM("World!");
+
 }
