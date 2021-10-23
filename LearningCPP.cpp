97e1180002a96b4c65cb22297e3cc512d4bd2234
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-10-24 00:07:01 +0800

    Update LearningCPP.cpp

--- LearningCPP.cpp
+++ LearningCPP.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
+#include <string>
 void increment(float& const value) {
 	value++;
 }
 // LEARNING Inheretence -> 3
-
+template <typename T>
+void Print(T var) {
+std::cout << var << std::endl;
+}
 class Entity {
 public: 
-	float x, y, speed;
-	void Move(float& xa, float& ya) {
-		x += xa * speed;
-		y += ya * speed;
+	float x;
+	float y;
+	float speed =1  ; // need deref pointer as cannot multiply/divide pointer by float
+
+
+	Entity() {};
+
+	void Move(float &xa, float &ya) {
+		x += xa * (speed);
+		y += ya * (speed); // need to dereference because memory address and contents r different
+
+		
+	}
+
+	 virtual void GetDetails() {
+		std::cout << x << " " << y << " " << speed << std::endl;
 	}
 };
 
 
 
 
-class Player: public Entity { 
+class Player : public Entity {
 //new data type eg int var it will be Player var has inherited position properties from entity class
 public:
 	const char* name;
 	Player(float X, float Y, float S, const char* Name) { // initilisation 
 		x = X;
-		y = Y;
+	    y = Y;
 		speed = S;
 		name = Name;
 		
 	}
+	void GetDetails() {
+		std::cout << x << " " << y << " " << speed << std::endl;
+	}
+
+
 
 };
 
@@ -69,6 +90,17 @@ int main() {
 
 	Player player(5.0, 5.0, 1.0, "Neil");
 	Player player2(2.0, 2.0, 1.0, "test");
+	Entity entity;
+	entity.x = 3.0;
+	entity.y = 3.0;
+
+std::cout << "ENV " << std::endl;
+	player.GetDetails();
+	player2.GetDetails();
+	entity.GetDetails();
+std::cout << "ENV " << std::endl;
+
+
 
 	
 	
@@ -86,7 +118,7 @@ int main() {
 	// Size of Player = Size of Entity(12 bytes) + Const Char Name (4bytes)
 	// Size of Char is 4 bytes instead of 1 because Name is int type (N->ASCII)
 
-
+	Print<std::string>("abc");
 	/// LEARNING ENUMS -> 2
 	Log log;
 	log.SetLevel(Log::Level::LError);
